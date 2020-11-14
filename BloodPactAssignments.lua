d5d446b6c5d2f07814dfa1d7c4da44772faf8d02
kloggson <kloggson@gmail.com>
2020-11-14 19:22:08 +0100

    test

--- BloodPactAssignments.lua
+++ BloodPactAssignments.lua
@@ -1,15 +1,36 @@
+
+
 -- addon frame
 local frame = BPAframe or CreateFrame("FRAME", "BPAframe")
+local classcolor = {["Priest"] = {1.00, 1.00, 1.00}, ["Paladin"] = {0.96, 0.55, 0.73}, ["Druid"] = {1.00, 0.49, 0.04}, ["Shaman"] = {0.96, 0.55, 0.73}, ["Warrior"] = {0.78, 0.61, 0.43}}
+potentialTanks = {}
+
+
+for i=1,40 do
+      local name, _, _, _, class, _, _, _, _, role, _, _ = GetRaidRosterInfo(i);
+      if class == "Warrior" or class == "Druid" then
+        tank = {"class" = class, "name" = name}
+        table.insert(potentialTanks, tank)
+      end
+   end
+   -- print all potential tanks
+   for i=1,#potentialTanks do
+     print(potentialTanks[i].name)
+     print(potentialTanks[i].class)
+   end
+
 
 -- Main ui window
 local BPA = BPA or CreateFrame("frame", "BPA")
 BPA:Hide()
+
 BPA:SetBackdrop({
-  bgFile = "Interface\\FrameGeneral\\UI-Background-Rock",
+  bgFile = "Interface\\AddOns\\BloodPactAssignments\\Resources\\backdrop",
   edgeFile = "Interface\\DialogFrame\\UI-DialogBox-Border",
   tile = false, tileSize = 32, edgeSize = 32,
   insets = {left = 11, right = 12, top = 12, bottom = 11}
 })
+BPA:SetBackdropColor(0,0,0,0.6)
 BPA:SetWidth(512)
 BPA:SetHeight(400)
 BPA:SetPoint("CENTER", UIParent)
@@ -24,6 +45,14 @@ BPA:SetFrameStrata("FULLSCREEN_DIALOG")
  BPA.closeButton:SetScript("OnClick", function(self)
    BPA:Hide()
   end)
+
+BPA.heal = BPA:CreateFontString(nil, "ARTWORK")
+BPA.heal:SetFontObject(GameFontNormalSmall)
+BPA.heal:SetPoint("TOP", 0 , - 12)
+BPA.heal:SetText("Blood Pact Raid Assignments")
+
+
+
 
 -- addon Load
 local function BPALoad(self, event, arg1, arg2, arg3, arg4, ...)
